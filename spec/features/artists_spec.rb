49a2952c47180cbdc9523d548b48e741cdaa3fb0
Hatice <haticekaraty@gmail.com>
2021-05-21 22:02:37 -0400

    commit tree f20d8bf380148fd209a6460569db53b7d99eda20

--- spec/features/artists_spec.rb
+++ spec/features/artists_spec.rb
@@ -1,7 +1,7 @@
 describe 'artists', type: :feature do
   before do
-    Artist.destroy_all
-    Song.destroy_all
+    #Artist.destroy_all
+    #Song.destroy_all
     @artist = Artist.create!(name: 'Daft Punk')
     @grid = @artist.songs.create!(title: 'The Grid')
     @voyager = @artist.songs.create!(title: 'Voyager')
